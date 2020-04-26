01a28e20eaa08494023a29ffef28ee3aa4c62b74
Dante Tobar <dante.m.tobar@gmail.com>
2020-04-26 14:31:52 -0700

    algorithm choise list

---
+++ src/components/AlgChoices.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './AlgChoices.css';
+
+const AlgChoices = props => {
+  return (
+    <div id="alg-choices">
+      <h2>Choose an Algorithm</h2>
+      <ul>
+        <li>Algorithm 1</li>
+
+        <li>Algorithm 2</li>
+
+        <li>Algorithm 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default AlgChoices;
