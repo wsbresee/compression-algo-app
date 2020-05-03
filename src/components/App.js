58c2adc673ebc824b475f9e521229d5bb27cf97c
Willy Breese <willy.bresee@ampermusic.com>
2020-05-03 12:05:06 -0400

    screwed up some stuff

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,26 @@ import axios from 'axios';
 import './App.css';
 import Topbar from './Topbar';
 import AlgChoices from './AlgChoices';
+import Parameter from './Parameter';
 import FileUpload from './FileUpload';
 import LoadingScreen from './LoadingScreen';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { algChoice: null, file: {}, results: {}, loading: false };
+    this.state = { algChoice: null, paramChoice: null, file: {}, results: {}, loading: false };
     this.chooseAlg = this.chooseAlg.bind(this);
+    this.chooseParam = this.chooseParam.bind(this);
     this.chooseFile = this.chooseFile.bind(this);
     this.compressFile = this.compressFile.bind(this);
   }
 
   chooseAlg(algChoice) {
     this.setState({ algChoice });
+  }
+
+  chooseParam(paramChoice) {
+    this.setState({ paramChoice });
   }
 
   chooseFile(file) {
@@ -26,10 +32,14 @@ class App extends React.Component {
   compressFile() {
     if (this.state.file.name) {
       const file = new Blob([this.state.file]);
+      const algChoice = this.state.algChoice
+      const paramChoice = this.state.paramChoice
 
       const formData = new FormData();
 
       formData.append('file', file, file.filename);
+      formData.append('algChoice', algChoice);
+      formData.append('paramChoice', paramChoice);
       this.setState({ loading: true });
       axios.post('/compress', formData, {}).then(({ data }) => {
         this.setState({ loading: false, results: data });
@@ -46,14 +56,20 @@ class App extends React.Component {
   render() {
     const algs = this.state.algs || [];
 
-    const { algChoice, file, loading } = this.state;
-    const { chooseAlg, chooseFile, compressFile } = this;
+    const { algChoice, paramChoice, file, loading } = this.state;
+    const { chooseAlg, chooseParam, chooseFile, compressFile } = this;
 
     return (
       <React.Fragment>
         <Topbar />
         <div className="contents">
-          <AlgChoices algs={algs} chooseAlg={chooseAlg} algChoice={algChoice} />
+          <div>
+            <AlgChoices algs={algs} chooseAlg={chooseAlg} algChoice={algChoice} />
+            <Parameter
+              chooseParam={chooseParam}
+              paramChoice={paramChoice}
+            />
+          </div>
           <FileUpload
             file={file}
             chooseFile={chooseFile}
